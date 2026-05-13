bec07ffae56910157c1f75cabf1c5b94baf99a89
Steven Marquez <stevenmarquez1999@gmail.com>
2026-05-13 16:27:07 -0400

    fixed alignment issue

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,8 @@ function About() {
   return (
     <div>
       <h1 style={{ marginLeft: '2rem' }}>About</h1>
-      <p>Email: yourname@email.com</p>
-      <p>Instagram: <a href="https://instagram.com/king_of_artists0w0" target="_blank" rel="noreferrer">@king_of_artists0w0</a></p>
+      <p style={{ marginLeft: '2rem' }}>Email: yourname@email.com</p>
+      <p style={{ marginLeft: '2rem' }}>Instagram: <a href="https://instagram.com/king_of_artists0w0" target="_blank" rel="noreferrer">@king_of_artists0w0</a></p>
     </div>
   );
 }
